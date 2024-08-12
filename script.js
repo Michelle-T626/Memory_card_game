933c6f0b0735749df50a00a711e6d558108aea60
Michelle_T <162118949+Michelle-T626@users.noreply.github.com>
2024-08-12 22:12:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const cards = document.querySelectorAll(".card");
 
+let cardOne, cardTwo;
+
 function flipCard(e) {
     let clickedCard = e.target; //getting user clicked card
-    clickedCard.classList.add("flip");
+    if(clickedCard !== cardOne) {
+        clickedCard.classList.add("flip");
+        if(!cardOne) {
+            //return the cardOne value to clickedCard
+            return cardOne = clickedCard;
+        }
+        cardTwo = clickedCard;
+
+        let cardOneImg = cardOne.querySelector("img").src,
+        cardTwoImg = cardTwo.querySelector("img").src;
+        matchCards(cardOneImg, cardTwoImg);
+    }
+}
+
+function matchCards(img1, img2) {
+    if(img1 === img2) { //if two img matched
+        return console.log("Card matched");
+    }
+    console.log("Card not matched");
 }
 
 cards.forEach(card => { //adding click event to all cards
     card.addEventListener("click", flipCard);
 });
 
+//17:40 from tutorial 
